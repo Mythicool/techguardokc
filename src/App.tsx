041563d4b9eb36f15f81696c18d233f768dc5be5
Mythicool <Johnvargin@gmail.com>
2025-07-19 10:49:13 -0500

    Remove Footer component to fix deployment accessibility issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
+
 
 function App() {
   return (
@@ -25,7 +25,7 @@ function App() {
         <Services />
         <About />
         <Contact />
-        <Footer />
+
       </div>
     </div>
   );
